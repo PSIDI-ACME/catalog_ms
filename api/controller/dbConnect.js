3c0392f55fcf86cffd9eb82066e2abb19337f448
RuiRLGomes <1141417@isep.ipp.pt>
2020-12-27 20:50:31 +0000

    TENTH COMMIT: Updating Database access Credentials

--- api/controller/dbConnect.js
+++ api/controller/dbConnect.js
@@ -1,10 +1,10 @@
 const { Pool, Client } = require('pg');
 
 const client = new Client({
-    user: 'xhxlvseuqrvtjf',
-    host: 'ec2-54-163-215-125.compute-1.amazonaws.com',
-    database: 'd8g80jlg8bo0i3',
-    password: 'f7ed5215c032f921b4c83aa3d10a9425be61a24f812d8fccd89e27d563082531',
+    user: 'xxltshilejseet',
+    host: 'ec2-54-247-94-127.eu-west-1.compute.amazonaws.com',
+    database: 'd7tqem71hsg2hk',
+    password: 'd0dc22d3afa3c396823a790052f65a0ae73ad7e35d1b0e5ee12d406fa21e247f',
     port: 5432,
     ssl: {rejectUnauthorized: false}
 })
